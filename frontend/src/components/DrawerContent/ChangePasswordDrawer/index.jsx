9b748faa25fa8e204ee9edf1f3be7b5c48fc18b0
Onat <53895969+lokutech@users.noreply.github.com>
2021-12-28 16:37:10 +0300

    My account page; change password hooked up to backend

--- frontend/src/components/DrawerContent/ChangePasswordDrawer/index.jsx
+++ frontend/src/components/DrawerContent/ChangePasswordDrawer/index.jsx
@@ -1,15 +1,42 @@
+import {useState} from 'react'
 import { Box, Typography, Button, Grid, TextField } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
-import Search from "../../Search";
+import { useUpdateChangeMyPassword } from "../../../graphql/updateChangeMyPassword";
+import { useSnackbar } from 'notistack';
 
-const ChangePasswordDrawer = () => {
+const ChangePasswordDrawer = ({handleClose}) => {
+    // GraphQL hook
+    const updateChangeMyPassword = useUpdateChangeMyPassword();
+
+    // State
+    const [password, setPassword] = useState(null)
+
+    const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+
+    // Submit new password
+    const handleSubmit =  async () => {
+        let response = await updateChangeMyPassword({ password })
+        if (response === 'success') {
+            closeSnackbar()
+            enqueueSnackbar(`Success`, { variant: "success" })
+            handleClose()
+        }else{
+            if (response.errors) {
+              response.errors.map(err => enqueueSnackbar(err.message, { variant: "error" }))
+            }
+            if (response.r === 'error') {
+                enqueueSnackbar(response.msg, { variant: "error" })
+            }
+        }
+    }
+
     return (
         <Box position="relative">
             <Box sx={{ p: "4.125rem" }}>
 
                 <Box position="absolute" top="26px" right="39px" display="flex" alignItems="center">
-                    <Button variant="text" startIcon={<FontAwesomeIcon icon={faTimes} />}>
+                    <Button variant="text" onClick={handleClose} style={{ paddingLeft: "16px", paddingRight: "16px"}} startIcon={<FontAwesomeIcon icon={faTimes} />}>
                         Close
                     </Button>
                 </Box>
@@ -20,11 +47,12 @@ const ChangePasswordDrawer = () => {
 
                 <Grid mt={3.4} display="flex" alignItems="center">
                     <TextField
+                        onChange={(e) => setPassword(e.target.value)}
                         label="New password"
                         id="new_password"
                         size="small"
                     />
-                    <Button variant="contained" color="primary" height="100%" sx={{ ml: 1 }} >Submit</Button>
+                    <Button  onClick={handleSubmit} variant="contained" color="primary" height="100%" sx={{ ml: 1 }} >Submit</Button>
                 </Grid>
             </Box>
         </Box>
--- frontend/src/pages/MemberDetail.jsx
+++ frontend/src/pages/MemberDetail.jsx
@@ -257,7 +257,7 @@ const MemberDetail = () => {
         </Box>
 
         <Drawer anchor="right" open={isOpenChangePassword} onClose={() => setIsOpenPassword(!isOpenChangePassword)} sx={drawerStyles}>
-            <ChangePasswordDrawer />
+            <ChangePasswordDrawer handleClose={() => setIsOpenPassword(false)} />
         </Drawer>
 
         <Drawer anchor="right" open={isOpenDeleteUser} onClose={() => setIsOpenDeleteUser(!isOpenDeleteUser)} sx={drawerStyles}>
