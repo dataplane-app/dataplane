059013a62f3ff518700cd1ebf4079cd6b6243a94
Onat <53895969+lokutech@users.noreply.github.com>
2022-01-05 23:23:09 +0300

    added availablePermissions GraphQL hook

---
+++ frontend/src/graphql/availablePermissions.js
@@ -0,0 +1,36 @@
+import { gql, GraphQLClient } from 'graphql-request';
+import { useGlobalAuthState } from '../Auth/UserAuth';
+
+const graphlqlEndpoint = process.env.REACT_APP_GRAPHQL_ENDPOINT_PRIVATE;
+
+const query = gql`
+    {
+        availablePermissions {
+            Code
+            Level
+            Label
+        }
+    }
+`;
+
+export const useAvailablePermissions = () => {
+    const authState = useGlobalAuthState();
+    const jwt = authState.authToken.get();
+
+    const headers = {
+        Authorization: 'Bearer ' + jwt,
+    };
+
+    const client = new GraphQLClient(graphlqlEndpoint, {
+        headers,
+    });
+
+    return async (input) => {
+        try {
+            const res = await client.request(query, input);
+            return res?.availablePermissions;
+        } catch (error) {
+            return JSON.parse(JSON.stringify(error, undefined, 2)).response;
+        }
+    };
+};
