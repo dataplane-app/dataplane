929a0155b0841c84ec99a34f94cf6fbfd1785aac
Onat <53895969+onattech@users.noreply.github.com>
2022-01-15 19:14:13 +0300

    merge

--- mainapp/graphql/private/resolvers/permissions.resolvers.go
+++ mainapp/graphql/private/resolvers/permissions.resolvers.go
@@ -484,19 +484,3 @@ func (r *queryResolver) GetAccessGroups(ctx context.Context, userID string, envi
 	}
 	return e, nil
 }
-
-func (r *queryResolver) GetAccessGroups(ctx context.Context, userID string, environmentID string) ([]*models.PermissionsAccessGroups, error) {
-
-	// NEEDS PERMISSIONS <==================
-
-	e := []*models.PermissionsAccessGroups{}
-
-	err := database.DBConn.Find(&e).Error
-	if err != nil {
-		if os.Getenv("debug") == "true" {
-			logging.PrintSecretsRedact(err)
-		}
-		return nil, errors.New("Retrive users database error.")
-	}
-	return e, nil
-}
