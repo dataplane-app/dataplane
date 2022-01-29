bc83e3c621ba8a5e35401c8b1065894f2484b698
Onat <53895969+onattech@users.noreply.github.com>
2022-01-29 18:18:07 +0300

    Worker groups front end hooked up to backend

---
+++ frontend/src/graphql/getWorkerGroups.js
@@ -0,0 +1,40 @@
+import { gql, GraphQLClient } from 'graphql-request';
+import { useGlobalAuthState } from '../Auth/UserAuth';
+
+const graphlqlEndpoint = process.env.REACT_APP_GRAPHQL_ENDPOINT_PRIVATE;
+
+const query = gql`
+    query getWorkerGroups($environmentName: String!) {
+        getWorkerGroups(environmentName: $environmentName) {
+            WorkerGroup
+            Status
+            T
+            Interval
+            Env
+            LB
+            WorkerType
+        }
+    }
+`;
+
+export const useGetWorkerGroups = () => {
+    const authState = useGlobalAuthState();
+    const jwt = authState.authToken.get();
+
+    const headers = {
+        Authorization: 'Bearer ' + jwt,
+    };
+
+    const client = new GraphQLClient(graphlqlEndpoint, {
+        headers,
+    });
+
+    return async (input) => {
+        try {
+            const res = await client.request(query, input);
+            return res?.getWorkerGroups;
+        } catch (error) {
+            return JSON.parse(JSON.stringify(error, undefined, 2)).response;
+        }
+    };
+};
--- frontend/src/pages/Workers/Workers.jsx
+++ frontend/src/pages/Workers/Workers.jsx
@@ -1,69 +1,65 @@
 import { useState, useMemo, useEffect } from 'react';
-import { Box, Grid, Typography, Button, Drawer } from '@mui/material';
+import { Box, Grid, Typography, Drawer } from '@mui/material';
 import Search from '../../components/Search';
 import { useTable, useGlobalFilter } from 'react-table';
 import { useHistory } from 'react-router-dom';
 import CustomChip from '../../components/CustomChip';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDocker } from '@fortawesome/free-brands-svg-icons';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { useGlobalEnvironmentState } from '../../components/EnviromentDropdown';
 import SecretsDrawer from '../../components/DrawerContent/SecretsDrawer';
-
-// import { useGetUsers } from '../graphql/getUsers';
-// import { useSnackbar } from 'notistack';
+import { useGetWorkerGroups } from '../../graphql/getWorkerGroups';
+import { useSnackbar } from 'notistack';
+import { useGlobalAuthState } from '../../Auth/UserAuth';
+import decode from 'jwt-decode';
 
 const tableWidth = '570px';
 
 export default function Workers() {
+    // React router
     let history = useHistory();
-    // const { enqueueSnackbar } = useSnackbar();
 
     // Global environment state with hookstate
     const Environment = useGlobalEnvironmentState();
 
     // Users state
-    const [data, setData] = useState([
-        {
-            id: '36474-6768-6768-67859',
-            name: 'Python 1',
-            description: 'Python workers for generic work loads.',
-            type: 'Docker',
-            workers: 3,
-            cpu: 1,
-            mb: 200,
-            lastUpdate: '2022-01-20T11:56:08Z',
-            balancer: 'Round robin',
-        },
-    ]);
+    const [data, setData] = useState([]);
 
-    // Sidebar states
-    const [isOpenChangePassword, setIsOpenPassword] = useState(false);
+    // Sidebar state
     const [isOpenSecrets, setIsOpenSecrets] = useState(false);
 
-    // Get workers on load
-    // const getUsers = useGetUsers();
-    useEffect(() => {
-        // retrieveUsers();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    // Control state
+    const [triggerRefresh, setTriggerRefresh] = useState(1);
 
-    // Get users
-    // const retrieveUsers = async () => {
-    //     let users = await getUsers();
-    //     !users.errors ? setData(users) : enqueueSnackbar('Unable to retrieve users', { variant: 'error' });
-    // };
+    // Custom hook
+    const getWorkerGroups = useGetWorkerGroups_(Environment.name.get(), setData);
+
+    // Get workers on load, on environment change and every 5 seconds
+    useEffect(() => {
+        getWorkerGroups();
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [Environment.name.get(), triggerRefresh]);
+
+    // Trigger refresh on get worker groups
+    useEffect(() => {
+        let triggerRefreshTimer = setInterval(() => {
+            setTriggerRefresh(triggerRefresh * -1);
+        }, 5000);
+
+        return () => clearInterval(triggerRefreshTimer);
+    }, [triggerRefresh]);
 
     const columns = useMemo(
         () => [
             {
                 Header: 'Member',
-                accessor: (row) => [row.name, row.description, row.type],
+                accessor: (row) => [properCase(row.WorkerGroup), properCase(row.WorkerType)],
                 Cell: (row) => <CustomWorker row={row} onClick={() => history.push(`/workers/${row.row.original.id}`)} setIsOpenSecrets={setIsOpenSecrets} />,
             },
             {
                 Header: 'Status',
-                accessor: (row) => [row.workers, row.balancer],
+                accessor: (row) => [row.Status, balancerDict[row.LB]],
                 Cell: (row) => <CustomStatus row={row} />,
             },
         ],
@@ -71,7 +67,7 @@ export default function Workers() {
     );
 
     // Use the state and functions returned from useTable to build your UI
-    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, state, setGlobalFilter } = useTable(
+    const { getTableProps, getTableBodyProps, rows, prepareRow, state, setGlobalFilter } = useTable(
         {
             columns,
             data,
@@ -87,7 +83,7 @@ export default function Workers() {
 
     return (
         <Box className="page">
-            <Typography component="h2" variant="h2" color="text.primary">
+            <Typography id="test" component="h2" variant="h2" color="text.primary">
                 Worker groups
             </Typography>
 
@@ -110,23 +106,6 @@ export default function Workers() {
             </Box>
 
             <Box component="table" mt={2} sx={{ width: tableWidth }} {...getTableProps()}>
-                {/* <thead>
-                    {headerGroups.map((headerGroup) => (
-                        <Box
-                            component="tr"
-                            display="grid"
-                            sx={{ '*:first-of-type': { ml: '22px' }, '*:last-child': { textAlign: 'center' } }}
-                            gridTemplateColumns="repeat(3, 1fr)"
-                            justifyContent="flex-start"
-                            {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map((column) => (
-                                <Box component="td" color="text.primary" fontWeight="600" fontSize="15px" textAlign="left" {...column.getHeaderProps()}>
-                                    {column.render('Header')}
-                                </Box>
-                            ))}
-                        </Box>
-                    ))}
-                </thead> */}
                 <Box component="tbody" display="flex" sx={{ flexDirection: 'column' }} {...getTableBodyProps()}>
                     {rows.map((row, i) => {
                         prepareRow(row);
@@ -168,7 +147,7 @@ export default function Workers() {
 }
 
 const CustomWorker = ({ row, onClick, setIsOpenSecrets }) => {
-    const [name, description, type] = row.value;
+    const [name, type] = row.value;
 
     return (
         <Grid container direction="column" mx="22px" alignItems="left" justifyContent="flex-start">
@@ -176,11 +155,10 @@ const CustomWorker = ({ row, onClick, setIsOpenSecrets }) => {
                 {name}
             </Typography>
             <Typography component="h5" variant="subtitle1" onClick={onClick} sx={{ cursor: 'pointer' }}>
-                {description}
+                Python workers for generic work loads.
             </Typography>
             <div style={{ display: 'flex', flexDirection: 'row' }}>
                 <Typography component="h5" mt={2} variant="subtitle1" style={{ fontSize: '17px', cursor: 'pointer' }} onClick={onClick}>
-                    <FontAwesomeIcon icon={faDocker} style={{ marginRight: 4 }} />
                     {type}
                 </Typography>
                 <Typography
@@ -198,19 +176,66 @@ const CustomWorker = ({ row, onClick, setIsOpenSecrets }) => {
 };
 
 const CustomStatus = ({ row }) => {
-    const [workers, balancer] = row.value;
+    const [status, LB] = row.value;
     return (
         <Grid container direction="column" alignItems="flex-end" pr={5}>
-            {workers > 0 ? <CustomChip label={workers + ' Online'} customColor="green" /> : <CustomChip label="Offline" customColor="red" />}
+            {status === 'Online' ? <CustomChip label={'Online'} customColor="green" /> : <CustomChip label="Offline" customColor="red" />}
 
             <div>
                 <Typography mt={3} variant="subtitle1" fontWeight="700">
                     Load balancer:
                 </Typography>
                 <Typography variant="subtitle1" align="left" sx={{ lineHeight: 1 }}>
-                    {balancer}
+                    {LB}
                 </Typography>
             </div>
         </Grid>
     );
+};
+
+// ------- Custom Hooks
+const useGetWorkerGroups_ = (environmentName, setWorkerGroups) => {
+    // GraphQL hook
+    const getAccessGroupUsers = useGetWorkerGroups();
+
+    const { enqueueSnackbar } = useSnackbar();
+
+    const auth = useGlobalAuthState();
+    let { exp } = decode(auth.authToken.get());
+
+    // Get worker groups
+    return async () => {
+        // Check if the token expired
+        if (exp * 1000 < new Date().valueOf()) {
+            enqueueSnackbar('Idle: not polling', { variant: 'warning' });
+            return;
+        }
+
+        const response = await getAccessGroupUsers({ environmentName });
+
+        if (response === null) {
+            setWorkerGroups([]);
+        } else if (response.r === 'error') {
+            enqueueSnackbar("Can't get worker groups: " + response.msg, { variant: 'error' });
+        } else if (response.r === 'Unauthorized') {
+            enqueueSnackbar('Idle: not polling', { variant: 'warning' });
+        } else if (response.errors) {
+            response.errors.map((err) => enqueueSnackbar(err.message + ': get worker groups failed', { variant: 'error' }));
+        } else {
+            setWorkerGroups(response);
+        }
+    };
+};
+
+// -------- Utility function
+/**
+ * @param {string} text
+ * @example 'python_1' => 'Python 1'
+ */
+function properCase(text) {
+    return (text[0].toUpperCase() + text.slice(1)).replace('_', ' ');
+}
+
+const balancerDict = {
+    roundrobin: 'Round robin',
 };
