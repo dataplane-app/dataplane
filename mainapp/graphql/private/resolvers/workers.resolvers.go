2bab39ed0b39d150d52ab7ef0ee59c93db1fd752
Onat <53895969+onattech@users.noreply.github.com>
2022-01-30 11:27:12 +0300

    Workers groups complete
    Backend -
    addSecretToWorkerGroup
    deleteSecretFromWorkerGroup
    getSecretWorkerGroups
    
    Frontend-
    Hooked up to backend

--- mainapp/graphql/private/resolvers/workers.resolvers.go
+++ mainapp/graphql/private/resolvers/workers.resolvers.go
@@ -13,9 +13,87 @@ import (
 	"dataplane/worker"
 	"encoding/json"
 	"errors"
+	"os"
 
 	"github.com/tidwall/buntdb"
 )
+
+func (r *mutationResolver) AddSecretToWorkerGroup(ctx context.Context, environmentName string, workerGroup string, secret string) (*string, error) {
+	currentUser := ctx.Value("currentUser").(string)
+	platformID := ctx.Value("platformID").(string)
+
+	e := models.Environment{}
+	database.DBConn.First(&e, "name = ?", environmentName)
+
+	// ----- Permissions
+	perms := []models.Permissions{
+		{Resource: "admin_platform", ResourceID: platformID, Access: "write", Subject: "user", SubjectID: currentUser, EnvironmentID: "d_platform"},
+		{Resource: "admin_environment", ResourceID: e.ID, Access: "write", Subject: "user", SubjectID: currentUser, EnvironmentID: e.ID},
+		{Resource: "environment_secrets", ResourceID: e.ID, Access: "read", Subject: "user", SubjectID: currentUser, EnvironmentID: e.ID},
+	}
+
+	permOutcome, _, _, _ := permissions.MultiplePermissionChecks(perms)
+
+	if permOutcome == "denied" {
+		return nil, errors.New("Requires permissions.")
+	}
+
+	workerSecret := models.WorkerSecrets{
+		SecretID:      secret,
+		WorkerGroupID: workerGroup,
+		Active:        true,
+	}
+
+	err := database.DBConn.Create(&workerSecret).Error
+
+	if err != nil {
+		if os.Getenv("debug") == "true" {
+			logging.PrintSecretsRedact(err)
+		}
+
+		return nil, errors.New("Add secret to worker group database error.")
+	}
+
+	response := "Secret added to work group"
+	return &response, nil
+}
+
+func (r *mutationResolver) DeleteSecretFromWorkerGroup(ctx context.Context, environmentName string, workerGroup string, secret string) (*string, error) {
+	currentUser := ctx.Value("currentUser").(string)
+	platformID := ctx.Value("platformID").(string)
+
+	e := models.Environment{}
+	database.DBConn.First(&e, "name = ?", environmentName)
+
+	// ----- Permissions
+	perms := []models.Permissions{
+		{Resource: "admin_platform", ResourceID: platformID, Access: "write", Subject: "user", SubjectID: currentUser, EnvironmentID: "d_platform"},
+		{Resource: "admin_environment", ResourceID: e.ID, Access: "write", Subject: "user", SubjectID: currentUser, EnvironmentID: e.ID},
+		{Resource: "environment_secrets", ResourceID: e.ID, Access: "read", Subject: "user", SubjectID: currentUser, EnvironmentID: e.ID},
+	}
+
+	permOutcome, _, _, _ := permissions.MultiplePermissionChecks(perms)
+
+	if permOutcome == "denied" {
+		return nil, errors.New("Requires permissions.")
+	}
+
+	workerSecret := models.WorkerSecrets{}
+
+	err := database.DBConn.Where(&models.WorkerSecrets{SecretID: secret, WorkerGroupID: workerGroup}).
+		Delete(&workerSecret).Error
+
+	if err != nil {
+		if os.Getenv("debug") == "true" {
+			logging.PrintSecretsRedact(err)
+		}
+
+		return nil, errors.New("Remove secret from worker group database error.")
+	}
+
+	response := "Secret removed from work group"
+	return &response, nil
+}
 
 func (r *queryResolver) GetWorkers(ctx context.Context, environmentName string) ([]*privategraphql.Workers, error) {
 	var resp []*privategraphql.Workers
@@ -118,4 +196,37 @@ func (r *queryResolver) GetWorkerGroups(ctx context.Context, environmentName str
 	})
 
 	return resp, nil
+}
+
+func (r *queryResolver) GetSecretGroups(ctx context.Context, environmentName string, secret string) ([]*models.WorkerSecrets, error) {
+	currentUser := ctx.Value("currentUser").(string)
+	platformID := ctx.Value("platformID").(string)
+
+	e := models.Environment{}
+	database.DBConn.First(&e, "name = ?", environmentName)
+
+	// ----- Permissions
+	perms := []models.Permissions{
+		{Resource: "admin_platform", ResourceID: platformID, Access: "write", Subject: "user", SubjectID: currentUser, EnvironmentID: "d_platform"},
+		{Resource: "admin_environment", ResourceID: e.ID, Access: "write", Subject: "user", SubjectID: currentUser, EnvironmentID: e.ID},
+		{Resource: "environment_secrets", ResourceID: e.ID, Access: "read", Subject: "user", SubjectID: currentUser, EnvironmentID: e.ID},
+	}
+
+	permOutcome, _, _, _ := permissions.MultiplePermissionChecks(perms)
+
+	if permOutcome == "denied" {
+		return nil, errors.New("Requires permissions.")
+	}
+
+	s := []*models.WorkerSecrets{}
+
+	err := database.DBConn.Where("secret_id = ?", secret).Find(&s).Error
+	if err != nil {
+		if os.Getenv("debug") == "true" {
+			logging.PrintSecretsRedact(err)
+		}
+		return nil, errors.New("Retrive users database error.")
+	}
+
+	return s, nil
 }
