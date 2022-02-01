2a610e3947a90c0fedee75982c0e53b630a910f9
Onat <53895969+onattech@users.noreply.github.com>
2022-02-01 14:14:11 +0300

    WS - Added reconnect and JWT token in uri

--- frontend/src/pages/Workers/useWebSocket.jsx
+++ frontend/src/pages/Workers/useWebSocket.jsx
@@ -1,19 +1,41 @@
 import { useEffect, useRef, useState } from 'react';
+import ConsoleLogHelper from '../../Helper/logger';
+import { useGlobalAuthState } from '../../Auth/UserAuth';
 
 export default function useWebSocket(workerId) {
     const [socketResponse, setSocketResponse] = useState([]);
+    const reconnectOnClose = useRef(true);
     const ws = useRef(null);
 
-    useEffect(() => {
-        ws.current = new WebSocket(`ws://localhost:9000/ws/workerstats/${workerId}`);
-        ws.current.onopen = () => console.log('ws opened');
-        ws.current.onclose = () => console.log('ws closed');
+    const { authToken } = useGlobalAuthState();
 
-        ws.current.onmessage = (e) => {
-            setSocketResponse(JSON.parse(e.data));
-        };
+    useEffect(() => {
+        function connect() {
+            ws.current = new WebSocket(`ws://localhost:9000/ws/workerstats/${workerId}?token=${authToken.get()}`);
+
+            ws.current.onopen = () => ConsoleLogHelper('ws opened');
+            ws.current.onclose = () => {
+                // Exit if closing the connection was intentional
+                if (!reconnectOnClose.current) {
+                    return;
+                }
+
+                // Reconnect on close
+                ConsoleLogHelper('ws trying to reconnect...');
+                setTimeout(function () {
+                    connect();
+                }, 1000);
+            };
+
+            ws.current.onmessage = (e) => {
+                setSocketResponse(JSON.parse(e.data));
+            };
+        }
+
+        connect();
 
         return () => {
+            reconnectOnClose.current = false;
             ws.current.close();
         };
     }, [workerId]);
