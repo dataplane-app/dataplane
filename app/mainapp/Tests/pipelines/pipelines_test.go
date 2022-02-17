4845849d98eeed98af6a055a4486f89b5382de1e
Onat <53895969+onattech@users.noreply.github.com>
2022-02-17 22:59:55 +0300

    Tests added for pipeline flow

--- app/mainapp/Tests/pipelines/pipelines_test.go
+++ app/mainapp/Tests/pipelines/pipelines_test.go
@@ -8,18 +8,23 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/bxcodec/faker/v3"
 	jsoniter "github.com/json-iterator/go"
 	"github.com/stretchr/testify/assert"
 )
 
 /*
 For individual tests - in separate window run: go run server.go
-go test -p 1 -v -count=1 -run TestAccessGroups dataplane/Tests/pipelines
+go test -p 1 -v -count=1 -run TestPipelines dataplane/Tests/pipelines
 * Login
 * Create pipeline
 * Get pipelines
+* Add pipeline flow
+* Update pipeline flow
+* Get pipeline flow
+* Delete pipeline flow
 */
-func TestAccessGroups(t *testing.T) {
+func TestPipelines(t *testing.T) {
 
 	database.DBConnect()
 
@@ -57,11 +62,13 @@ func TestAccessGroups(t *testing.T) {
 		envID = "test-environment-id"
 	}
 
+	pipelineId := testutils.TextEscape(faker.UUIDHyphenated())
+
 	// -------- Create pipeline -------------
 
 	mutation := `mutation {
 		addPipeline(
-			name: "Test_Pipeline",
+			name: "test_` + pipelineId + `",
 			environmentID: "` + envID + `",
 			description: "Test"
 			)
@@ -102,4 +109,199 @@ func TestAccessGroups(t *testing.T) {
 	}
 
 	assert.Equalf(t, http.StatusOK, httpResponse.StatusCode, "Get pipelines 200 status code")
+
+	// -------- Add pipeline flow -------------
+	mutation = `mutation {
+		addUpdatePipelineFlow(
+			environmentID: "` + envID + `",
+			pipelineID: "test_` + pipelineId + `",
+			input:{
+				nodesInput: [{
+					nodeID: "nodeID",
+					name: "Name",
+					nodeType: "type",
+					description: "desc",
+					meta: {
+					  position: {
+						x: 75,
+						y: 315
+					  },
+					  data: {
+						language: "Bash"
+					  }
+					},
+					active: false
+				  },
+				  {
+					nodeID: "nodeID2",
+					name: "Name",
+					nodeType: "type",
+					description: "desc",
+					meta: {
+					  position: {
+						x: 75,
+						y: 315
+					  },
+					  data: {
+						language: "Bash"
+					  }
+					},
+					active: false
+				  }
+				  ],
+				  edgesInput: [{
+					edgeID: "edgeID",
+					from: "from",
+					to: "to",
+					meta: {
+					  sourceHandle: "sourceHandle",
+					  targetHandle: "targetHandle",
+					  edgeType: "custom",
+					  arrowHeadType: "arrowHeadType"
+					},
+					active: false
+				  }]
+				}
+			)
+      		
+		}`
+
+	response, httpResponse = testutils.GraphQLRequestPrivate(mutation, accessToken, "{}", graphQLUrlPrivate, t)
+
+	log.Println(string(response))
+
+	if strings.Contains(string(response), `"errors":`) {
+		t.Errorf("Error in graphql response")
+	}
+
+	assert.Equalf(t, http.StatusOK, httpResponse.StatusCode, "Add pipeline flow 200 status code")
+
+	// -------- Update pipeline flow -------------
+	mutation = `mutation {
+		addUpdatePipelineFlow(
+			environmentID: "` + envID + `",
+			pipelineID: "test_` + pipelineId + `",
+			input:{
+				nodesInput: [{
+					nodeID: "nodeID",
+					name: "Name2",
+					nodeType: "type",
+					description: "desc",
+					meta: {
+					  position: {
+						x: 75,
+						y: 315
+					  },
+					  data: {
+						language: "Bash"
+					  }
+					},
+					active: false
+				  },
+				  {
+					nodeID: "nodeID2",
+					name: "Name2",
+					nodeType: "type",
+					description: "desc",
+					meta: {
+					  position: {
+						x: 75,
+						y: 315
+					  },
+					  data: {
+						language: "Bash"
+					  }
+					},
+					active: false
+				  }
+				  ],
+				  edgesInput: [{
+					edgeID: "edgeID",
+					from: "from2",
+					to: "to2",
+					meta: {
+					  sourceHandle: "sourceHandle",
+					  targetHandle: "targetHandle",
+					  edgeType: "custom",
+					  arrowHeadType: "arrowHeadType"
+					},
+					active: false
+				  }]
+				}
+			)
+      		
+		}`
+
+	response, httpResponse = testutils.GraphQLRequestPrivate(mutation, accessToken, "{}", graphQLUrlPrivate, t)
+
+	log.Println(string(response))
+
+	if strings.Contains(string(response), `"errors":`) {
+		t.Errorf("Error in graphql response")
+	}
+
+	assert.Equalf(t, http.StatusOK, httpResponse.StatusCode, "Update pipeline 200 status code")
+
+	// -------- Get pipeline flow -------------
+
+	query = `query {
+		getPipelineFlow(
+			environmentID: "` + envID + `",
+			pipelineID: "test_` + pipelineId + `",
+			){
+				edges {
+					edgeID
+					pipelineID
+					from
+					to
+					environmentID
+					active
+					meta
+				}
+				nodes {
+					nodeID
+					pipelineID
+					name
+					environmentID
+					nodeType
+					description
+					active
+					meta
+				}
+			}
+		}`
+
+	response, httpResponse = testutils.GraphQLRequestPrivate(query, accessToken, "{}", graphQLUrlPrivate, t)
+
+	log.Println(string(response))
+
+	if strings.Contains(string(response), `"errors":`) {
+		t.Errorf("Error in graphql response")
+	}
+
+	assert.Equalf(t, http.StatusOK, httpResponse.StatusCode, "Get pipeline flow 200 status code")
+
+	// -------- Delete pipeline flow -------------
+	mutation = `mutation {
+		addUpdatePipelineFlow(
+			environmentID: "` + envID + `",
+			pipelineID: "test_` + pipelineId + `",
+			input:{
+				nodesInput: [],
+				edgesInput: []
+				}
+			)
+      		
+		}`
+
+	response, httpResponse = testutils.GraphQLRequestPrivate(mutation, accessToken, "{}", graphQLUrlPrivate, t)
+
+	log.Println(string(response))
+
+	if strings.Contains(string(response), `"errors":`) {
+		t.Errorf("Error in graphql response")
+	}
+
+	assert.Equalf(t, http.StatusOK, httpResponse.StatusCode, "Delete pipeline 200 status code")
+
 }
