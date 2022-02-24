9f625cc39f9666d23afc6f76c70e1782e6f1dbf4
Onat <53895969+onattech@users.noreply.github.com>
2022-02-24 23:51:29 +0300

    Added run/stop pipeline flows.

---
+++ app/mainapp/graphql/private/resolvers/runpipelines.resolvers.go
@@ -0,0 +1,17 @@
+package privateresolvers
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+)
+
+func (r *mutationResolver) RunPipelines(ctx context.Context, userID string, pipelineID string, environmentID string) (string, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) StopPipelines(ctx context.Context, userID string, pipelineID string, environmentID string) (string, error) {
+	panic(fmt.Errorf("not implemented"))
+}
