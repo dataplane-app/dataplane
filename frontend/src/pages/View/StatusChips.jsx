18244ae37fc4927e228ec55a73b8cf8282803c48
Onat <53895969+onattech@users.noreply.github.com>
2022-02-27 19:14:02 +0300

    Chips show 0 value.

--- frontend/src/pages/View/StatusChips.jsx
+++ frontend/src/pages/View/StatusChips.jsx
@@ -10,11 +10,11 @@ export default function StatusChips() {
 
     return (
         <Grid item display="flex" alignItems="center" sx={{ alignSelf: 'center' }} flex={1.2}>
-            <CustomChip amount={Object.values(FlowState.elements.get()).filter((a) => a.type !== 'custom').length} label="Steps" margin={2} customColor="orange" />
-            <CustomChip amount={Object.values(RunState.get()).filter((a) => a === 'Queue').length} label="Queue" margin={2} customColor="purple" />
-            <CustomChip amount={Object.values(RunState.get()).filter((a) => a === 'Run').length} label="Running" margin={2} customColor="blue" />
-            <CustomChip amount={Object.values(RunState.get()).filter((a) => a === 'Success').length} label="Succeeded" margin={2} customColor="green" />
-            <CustomChip amount={Object.values(RunState.get()).filter((a) => a === 'Fail').length} label="Failed" margin={2} customColor="red" />
+            <CustomChip amount={Object.values(FlowState.elements.get()).filter((a) => a.type !== 'custom').length || '0'} label="Steps" margin={2} customColor="orange" />
+            <CustomChip amount={Object.values(RunState.get()).filter((a) => a === 'Queue').length || '0'} label="Queue" margin={2} customColor="purple" />
+            <CustomChip amount={Object.values(RunState.get()).filter((a) => a === 'Run').length || '0'} label="Running" margin={2} customColor="blue" />
+            <CustomChip amount={Object.values(RunState.get()).filter((a) => a === 'Success').length || '0'} label="Succeeded" margin={2} customColor="green" />
+            <CustomChip amount={Object.values(RunState.get()).filter((a) => a === 'Fail').length || '0'} label="Failed" margin={2} customColor="red" />
         </Grid>
     );
 }
