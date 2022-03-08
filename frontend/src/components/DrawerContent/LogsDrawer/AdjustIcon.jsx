c1475e0ae41ebffa14fc714dec7e2fbcdb0a20dc
Onat <53895969+onattech@users.noreply.github.com>
2022-03-08 19:49:40 +0300

    clean up

--- frontend/src/components/DrawerContent/LogsDrawer/AdjustIcon.jsx
+++ frontend/src/components/DrawerContent/LogsDrawer/AdjustIcon.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export function AdjustIcon() {
+export function RunningSpinner() {
     return (
         <svg width="16" height="16" fill="none" className="anim-rotate" xmlns="http://www.w3.org/2000/svg">
             <path opacity=".5" d="M8 15A7 7 0 108 1a7 7 0 000 14v0z" stroke="#65BEFF    " stroke-width="2"></path>
