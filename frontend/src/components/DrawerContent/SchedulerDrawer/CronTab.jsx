31fb576cc8cce4f7e64289ada1bce0decc6b3318
Onat <53895969+onattech@users.noreply.github.com>
2022-03-13 12:56:27 +0300

    fix timezone bug

--- frontend/src/components/DrawerContent/SchedulerDrawer/CronTab.jsx
+++ frontend/src/components/DrawerContent/SchedulerDrawer/CronTab.jsx
@@ -98,13 +98,15 @@ export function CronTab({ setValidationError, scheduleStatement, setScheduleStat
                                                     </TableCell>
                                                 </Tooltip>
                                             ) : null}
-                                            <Tooltip
-                                                title={formatUsersTime(row, userTimezone, timezone) + ' ' + userTimezone + ' ' + getTimeZoneOffSet(userTimezone)}
-                                                placement="top-start">
-                                                <TableCell component="th" scope="row">
-                                                    {timezone && timezone + ' ' + getTimeZoneOffSet(timezone)}
-                                                </TableCell>
-                                            </Tooltip>
+                                            {userTimezone ? (
+                                                <Tooltip
+                                                    title={formatUsersTime(row, userTimezone, timezone) + ' ' + userTimezone + ' ' + getTimeZoneOffSet(userTimezone)}
+                                                    placement="top-start">
+                                                    <TableCell component="th" scope="row">
+                                                        {timezone && timezone + ' ' + getTimeZoneOffSet(timezone)}
+                                                    </TableCell>
+                                                </Tooltip>
+                                            ) : null}
                                         </TableRow>
                                     );
                                 })}
