67b4351621667dca7dcaa9d99752a3f232e30d96
saul-gush <admin@gush.ai>
2022-03-13 00:42:33 +0000

    Fixing concurrent maps

--- app/go.mod
+++ app/go.mod
@@ -81,6 +81,7 @@ require (
 	github.com/nats-io/nats-server/v2 v2.7.3 // indirect
 	github.com/nats-io/nkeys v0.3.0 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
+	github.com/orcaman/concurrent-map v1.0.0 // indirect
 	github.com/pieterclaerhout/go-formatter v1.0.5 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
--- app/mainapp/utilities/loadbalance_roundrobin.go
+++ app/mainapp/utilities/loadbalance_roundrobin.go
@@ -1,22 +1,40 @@
 package utilities
 
-import "dataplane/mainapp/database/models"
+import (
+	"dataplane/mainapp/database/models"
+
+	cmap "github.com/orcaman/concurrent-map"
+)
 
 // var i = [string]int
-var i = make(map[string]int)
+// var i = make(map[string]int)
+// moved from map for concurrent use
+var i = cmap.New()
 
 // var servers = []string{"127.0.0.1:8000", "127.0.0.1:8001", "127.0.0.1:8003"}
 
 // Balance returns one of the servers based using round-robin algorithm
 func Balance(workers []models.WorkerStats, workerGroup string) string {
-	worker := workers[i[workerGroup]]
-	i[workerGroup]++
+
+	// Retrieve current count from map.
+	var count int
+	if tmp, ok := i.Get(workerGroup); ok {
+		count = tmp.(int)
+	}
+
+	// worker := workers[i[workerGroup]]
+	worker := workers[count]
+	count++
+	// i[workerGroup]++
+
+	i.Set(workerGroup, count)
 
 	// it means that we reached the end of servers
 	// and we need to reset the counter and start
 	// from the beginning
-	if i[workerGroup] >= len(workers) {
-		i[workerGroup] = 0
+	if count >= len(workers) {
+		// i[workerGroup] = 0
+		i.Set(workerGroup, 0)
 	}
 
 	// return the worker to send the task to
