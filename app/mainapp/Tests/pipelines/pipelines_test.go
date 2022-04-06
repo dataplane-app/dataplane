a0e1f68119364fd8b6cd1e5e011144524f8fbba2
Onat <53895969+onattech@users.noreply.github.com>
2022-04-07 01:28:02 +0300

    Pipeline tests not deleting pipeline issue fixed.

--- app/mainapp/Tests/pipelines/pipelines_test.go
+++ app/mainapp/Tests/pipelines/pipelines_test.go
@@ -66,7 +66,7 @@ func TestPipelines(t *testing.T) {
 		envID = "test-environment-id"
 	}
 
-	pipelineId := testutils.TextEscape(faker.UUIDHyphenated())
+	pipelineName := testutils.TextEscape(faker.UUIDHyphenated())
 
 	// -------- clean data -------
 	database.DBConn.Where("environment_id =?", envID).Delete(&models.PipelineNodes{})
@@ -76,7 +76,7 @@ func TestPipelines(t *testing.T) {
 
 	mutation := `mutation {
 		addPipeline(
-			name: "test_` + pipelineId + `",
+			name: "test_` + pipelineName + `",
 			environmentID: "` + envID + `",
 			description: "Test",
 			workerGroup: "python_1"
@@ -94,12 +94,12 @@ func TestPipelines(t *testing.T) {
 	assert.Equalf(t, http.StatusOK, httpResponse.StatusCode, "Create pipeline 200 status code")
 
 	// -------- Update pipeline -------------
-	id := jsoniter.Get(response, "data", "addPipeline").ToString()
+	pipelineId := jsoniter.Get(response, "data", "addPipeline").ToString()
 
 	mutation = `mutation {
 		updatePipeline(
-				name: "test_` + pipelineId + `",
-				pipelineID: "` + id + `",
+				name: "test_` + pipelineName + `",
+				pipelineID: "` + pipelineId + `",
 				environmentID: "` + envID + `",
 				description: "Test new description",
 				workerGroup: "python_1"
@@ -173,7 +173,7 @@ func TestPipelines(t *testing.T) {
 	mutation = `mutation {
 		addUpdatePipelineFlow(
 			environmentID: "` + envID + `",
-			pipelineID: "test_` + pipelineId + `",
+			pipelineID: "` + pipelineId + `",
 			input:{
 				nodesInput: [{
 					nodeID: "nodeID",
@@ -258,7 +258,7 @@ func TestPipelines(t *testing.T) {
 	mutation = `mutation {
 		addUpdatePipelineFlow(
 			environmentID: "` + envID + `",
-			pipelineID: "test_` + pipelineId + `",
+			pipelineID: "` + pipelineId + `",
 			input:{
 				nodesInput: [{
 					nodeID: "nodeID",
@@ -352,7 +352,7 @@ func TestPipelines(t *testing.T) {
 	query = `query {
 		getPipelineFlow(
 			environmentID: "` + envID + `",
-			pipelineID: "test_` + pipelineId + `",
+			pipelineID: "` + pipelineId + `",
 			){
 				edges {
 					edgeID
@@ -392,7 +392,7 @@ func TestPipelines(t *testing.T) {
 	mutation = `mutation {
 		addUpdatePipelineFlow(
 			environmentID: "` + envID + `",
-			pipelineID: "test_` + pipelineId + `",
+			pipelineID: "` + pipelineId + `",
 			input:{
 				nodesInput: [],
 				edgesInput: [],
@@ -417,7 +417,7 @@ func TestPipelines(t *testing.T) {
 		turnOnOffPipeline(
 				environmentID: "` + envID + `",
 				online: false,
-				pipelineID: "test_` + pipelineId + `")
+				pipelineID: "` + pipelineId + `")
 				  
 			}`
 
@@ -435,7 +435,7 @@ func TestPipelines(t *testing.T) {
 	mutation = `mutation {
 		deletePipeline(
 				environmentID: "` + envID + `",
-				pipelineID: "test_` + pipelineId + `")
+				pipelineID: "` + pipelineId + `")
 				  
 			}`
 
