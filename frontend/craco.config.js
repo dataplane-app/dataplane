58c6583b78deb613fb7e8d2b47eedf79f07a7924
Nicolas Marqui <nick.marqui@gmail.com>
2022-04-30 00:31:19 -0300

    Update craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -3,5 +3,13 @@ const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 module.exports = {
     webpack: {
         plugins: [new MonacoWebpackPlugin()],
+        configure: (webpackConfig, { env, paths }) => {
+            webpackConfig.output.filename = 'webapp/static/js/bundle.js';
+            webpackConfig.output.chunkFilename = 'webapp/static/js/[name].chunk.js';
+
+            //webpackConfig.output.publicPath = '/webapp/';
+
+            return webpackConfig;
+        },
     },
 };
