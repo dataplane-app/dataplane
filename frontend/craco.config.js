4a60f9da97ffb25331168c951e3d4dfe06545380
saul-gush <admin@gush.ai>
2022-05-09 13:59:18 +0100

    Saul testing local

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -2,6 +2,8 @@ const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 module.exports = {
     webpack: {
-        plugins: [new MonacoWebpackPlugin({publicPath: '/webapp/', languages: ['json', 'python', 'shell', 'python']})],
+        plugins: [new MonacoWebpackPlugin({ publicPath: '/webapp/', languages: ['json', 'python', 'shell', 'python']})],
     },
 };
+
+// publicPath: '/webapp',
