51ece7c78864181b257ff93fc355795e72c6500b
saul-gush <admin@gush.ai>
2022-05-09 15:11:23 +0100

    fix for code editor

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -2,6 +2,10 @@ const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 module.exports = {
     webpack: {
+        // output: {
+        //     // path: path.resolve(__dirname, 'build'),
+        //     publicPath: '/webapp',
+        //   },
         plugins: [new MonacoWebpackPlugin({ publicPath: '/webapp/', languages: ['json', 'python', 'shell', 'python']})],
     },
 };
