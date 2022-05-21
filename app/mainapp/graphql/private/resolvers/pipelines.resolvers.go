539f98dc93d75ac8bb895123c8d85ccb134cd6cd
saul-gush <admin@gush.ai>
2022-05-21 16:09:13 +0100

    fixed showing pipelines

--- app/mainapp/graphql/private/resolvers/pipelines.resolvers.go
+++ app/mainapp/graphql/private/resolvers/pipelines.resolvers.go
@@ -5,7 +5,7 @@ package privateresolvers
 
 import (
 	"context"
-	"dataplane/mainapp/auth_permissions"
+	permissions "dataplane/mainapp/auth_permissions"
 	"dataplane/mainapp/code_editor/filesystem"
 	"dataplane/mainapp/config"
 	"dataplane/mainapp/database"
@@ -1399,8 +1399,8 @@ inner join (
 		permissions p, permissions_accessg_users agu
 		where 
 		p.subject = 'access_group' and 
-		p.subject_id = agu.user_id and
-		p.subject_id = ? and
+		p.subject_id = agu.access_group_id and
+		agu.user_id = ? and
 		p.resource = 'specific_pipeline' and
 		p.environment_id = agu.environment_id and 
 		p.active = true and
@@ -1414,7 +1414,7 @@ where
 a.environment_id = ?
 order by a.created_at desc`
 
-		err := database.DBConn.Raw(
+		err := database.DBConn.Debug().Raw(
 			query, currentUser, currentUser, environmentID).Scan(&p).Error
 
 		if err != nil {
