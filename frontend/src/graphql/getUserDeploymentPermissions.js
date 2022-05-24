a920f8386165f1c0e1a7b6bfd6741ec953e25cef
Onat <53895969+onattech@users.noreply.github.com>
2022-05-24 21:45:46 +0300

    added userDeploymentPermissions and hookup to front end

---
+++ frontend/src/graphql/getUserDeploymentPermissions.js
@@ -0,0 +1,46 @@
+import { gql, GraphQLClient } from 'graphql-request';
+import { useGlobalAuthState } from '../Auth/UserAuth';
+
+const graphlqlEndpoint = process.env.REACT_APP_GRAPHQL_ENDPOINT_PRIVATE;
+
+const query = gql`
+    query userDeploymentPermissions($userID: String!, $environmentID: String!) {
+        userDeploymentPermissions(userID: $userID, environmentID: $environmentID) {
+            Access
+            Subject
+            SubjectID
+            PipelineName
+            ResourceID
+            EnvironmentID
+            Active
+            Level
+            Label
+            FirstName
+            LastName
+            Email
+            JobTitle
+        }
+    }
+`;
+
+export const useGetUserDeploymentPermissions = () => {
+    const authState = useGlobalAuthState();
+    const jwt = authState.authToken.get();
+
+    const headers = {
+        Authorization: 'Bearer ' + jwt,
+    };
+
+    const client = new GraphQLClient(graphlqlEndpoint, {
+        headers,
+    });
+
+    return async (input) => {
+        try {
+            const res = await client.request(query, input);
+            return res?.userDeploymentPermissions;
+        } catch (error) {
+            return JSON.parse(JSON.stringify(error, undefined, 2)).response;
+        }
+    };
+};
