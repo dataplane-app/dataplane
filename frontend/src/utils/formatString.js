19bec20c29399545d5962bf81ea8808105dbf35c
Onat <53895969+onattech@users.noreply.github.com>
2022-06-05 16:04:46 +0300

    Update formatString.js

--- frontend/src/utils/formatString.js
+++ frontend/src/utils/formatString.js
@@ -1,6 +1,6 @@
 export function formatSpecialPermission(permission) {
     const pipelineOrDeployment = permission.Label.split(' ')[0].replace('-', '');
-    const name = permission.PipelineName || permission.FirstName;
+    const name = permission.PipelineName;
     const access = `[${permission.Access.replaceAll(',', ', ')}]`;
     const accessGroupName = permission.Subject === 'access_group' ? ` (${permission.FirstName})` : '';
     return pipelineOrDeployment + ' ' + name + ' ' + access + accessGroupName;
