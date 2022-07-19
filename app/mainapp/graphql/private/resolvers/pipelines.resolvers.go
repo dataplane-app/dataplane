f833209c56d59cd42a358afe0ae0c959236f409a
Onat <53895969+onattech@users.noreply.github.com>
2022-07-19 14:49:52 +0300

    Fixed environment_edit_all_pipelines for AddPipeline resolver

--- app/mainapp/graphql/private/resolvers/pipelines.resolvers.go
+++ app/mainapp/graphql/private/resolvers/pipelines.resolvers.go
@@ -36,18 +36,17 @@ func (r *mutationResolver) AddPipeline(ctx context.Context, name string, environ
 	perms := []models.Permissions{
 		{Subject: "user", SubjectID: currentUser, Resource: "admin_platform", ResourceID: platformID, Access: "write", EnvironmentID: "d_platform"},
 		{Subject: "user", SubjectID: currentUser, Resource: "admin_environment", ResourceID: environmentID, Access: "write", EnvironmentID: environmentID},
-		{Subject: "user", SubjectID: currentUser, Resource: "environment_edit_all_pipelines", ResourceID: platformID, Access: "write", EnvironmentID: environmentID},
+		{Subject: "user", SubjectID: currentUser, Resource: "environment_edit_all_pipelines", ResourceID: environmentID, Access: "write", EnvironmentID: environmentID},
 		{Subject: "user", SubjectID: currentUser, Resource: "environment_create_pipelines", ResourceID: environmentID, Access: "write", EnvironmentID: environmentID},
 	}
 
-	permOutcome, _, _, _ := permissions.MultiplePermissionChecks(perms)
+	permOutcome, outcomes, _, _ := permissions.MultiplePermissionChecks(perms)
 
-	// envPipelines := "no"
-	// for _, outcome := range outcomes {
-	// 	if outcome.Perm.Resource == "environment_all_pipelines" && outcome.Result == "grant" {
-	// 		envPipelines = "yes"
-	// 	}
-	// }
+	for _, outcome := range outcomes {
+		if outcome.Perm.Resource == "environment_edit_all_pipelines" && outcome.Result == "grant" {
+			permOutcome = "yes"
+		}
+	}
 
 	if permOutcome == "denied" {
 		return "", errors.New("Requires permissions.")
