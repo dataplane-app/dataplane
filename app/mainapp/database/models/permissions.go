4065a22750fe08b2c09d88ddc4e8d2dc121ea2e3
Onat <53895969+onattech@users.noreply.github.com>
2022-07-19 11:32:01 +0300

    Added environment_create_pipeline permission

--- app/mainapp/database/models/permissions.go
+++ app/mainapp/database/models/permissions.go
@@ -45,6 +45,7 @@ var ResourceType = []*ResourceTypeStruct{
 	{Code: "environment_all_pipelines", Level: "environment", Label: "View all pipelines", Access: "read"},
 	{Code: "environment_edit_all_pipelines", Level: "environment", Label: "Edit all pipelines", Access: "write"},
 	{Code: "environment_run_all_pipelines", Level: "environment", Label: "Run all pipelines", Access: "write"},
+	{Code: "environment_create_pipelines", Level: "environment", Label: "Create pipelines", Access: "write"},
 
 	{Code: "environment_secrets", Level: "environment", Label: "Manage secrets", Access: "write"},
 	{Code: "environment_edit_workers", Level: "environment", Label: "Manage workers", Access: "write"},
