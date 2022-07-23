f5cb0a40eb5ef711bd77d6e3553588b4e27d5415
Onat <53895969+onattech@users.noreply.github.com>
2022-07-24 00:19:57 +0300

    Added permission-edit-all-pipelines cypress test

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -15,7 +15,7 @@ module.exports = defineConfig({
       "cypress/e2e/dataplane/admin-permission.cy.js",
       "cypress/e2e/dataplane/create-pipeline.cy.js",
       "cypress/e2e/dataplane/permission-manage-pipelines.cy.js",
-      // "cypress/e2e/dataplane/edit-pipeline.js",
+      "cypress/e2e/dataplane/permission-edit-all-pipelines.cy.js",
     ]
   },
 });
