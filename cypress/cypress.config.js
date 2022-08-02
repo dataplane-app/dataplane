f094d0d373e5bb1d45fca7ea2876624b1fcc7cff
Onat <53895969+onattech@users.noreply.github.com>
2022-08-02 23:30:50 +0300

    Added cypress test for access groups permissions
    Tests for Edit all pipelines permissions

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -1,22 +1,23 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  projectId: "mxdzm2",
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    projectId: 'mxdzm2',
+    e2e: {
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+        specPattern: [
+            'cypress/e2e/dataplane/01-get-started.cy.js',
+            'cypress/e2e/dataplane/02-add-remove-accessGroup.cy.js',
+            'cypress/e2e/dataplane/03-add-remove-environment.cy.js',
+            'cypress/e2e/dataplane/04-add-remove-permissions.cy.js',
+            'cypress/e2e/dataplane/05-add-remove-secret.cy.js',
+            'cypress/e2e/dataplane/06-add-remove-user.cy.js',
+            'cypress/e2e/dataplane/07-admin-permission.cy.js',
+            'cypress/e2e/dataplane/08-create-pipeline.cy.js',
+            'cypress/e2e/dataplane/09-permission-manage-pipelines.cy.js',
+            'cypress/e2e/dataplane/10-permission-edit-all-pipelines.cy.js',
+            'cypress/e2e/dataplane/11-permission-edit-all-pipelines-access-groups.cy.js',
+        ],
     },
-    specPattern: [
-      "cypress/e2e/dataplane/01-get-started.cy.js",
-      "cypress/e2e/dataplane/02-add-remove-accessGroup.cy.js",
-      "cypress/e2e/dataplane/03-add-remove-environment.cy.js",
-      "cypress/e2e/dataplane/04-add-remove-permissions.cy.js",
-      "cypress/e2e/dataplane/05-add-remove-secret.cy.js",
-      "cypress/e2e/dataplane/06-add-remove-user.cy.js",
-      "cypress/e2e/dataplane/07-admin-permission.cy.js",
-      "cypress/e2e/dataplane/08-create-pipeline.cy.js",
-      "cypress/e2e/dataplane/09-permission-manage-pipelines.cy.js",
-      "cypress/e2e/dataplane/10-permission-edit-all-pipelines.cy.js",
-    ]
-  },
 });
