31217b874d114acf58b27a6bda8fb2307fe595f2
Onat <53895969+onattech@users.noreply.github.com>
2022-09-18 14:06:31 +0300

    clean up

--- frontend/src/components/EditorPage/EditorColumn/index.jsx
+++ frontend/src/components/EditorPage/EditorColumn/index.jsx
@@ -292,6 +292,8 @@ const EditorColumn = forwardRef(({ children, ...rest }, ref) => {
                                                 style={{ marginLeft: 0, paddingLeft: 12 }}>
                                                 {/*  */}
                                                 {tabs.diffValue && tabs.content !== tabs.diffValue && (
+                                                    <Box sx={{ width: 8, height: 8, marginRight: 1, backgroundColor: 'secondary.main', borderRadius: '50%' }} />
+                                                )}
                                                 <Box component={FontAwesomeIcon} icon={faTimes} sx={{ fontSize: 13 }} color="editorPage.fileManagerIcon" />
                                             </Box>
                                         }
