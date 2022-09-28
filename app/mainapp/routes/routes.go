14d996f73bd5f6d29fea642fbcb9764c8d9810dc
Onat <53895969+onattech@users.noreply.github.com>
2022-09-28 19:32:03 +0300

    Api path updated

--- app/mainapp/routes/routes.go
+++ app/mainapp/routes/routes.go
@@ -428,7 +428,7 @@ func Setup(port string) *fiber.App {
 	})
 
 	// Pipeline API Trigger public
-	app.Post("/app/public/api-trigger/:id", auth.ApiAuthMiddle("public"), func(c *fiber.Ctx) error {
+	app.Post("/publicapi/api-trigger/:id", auth.ApiAuthMiddle("public"), func(c *fiber.Ctx) error {
 		c.Accepts("application/json")
 		pipelineID := c.Locals("pipelineID").(string)
 		environmentID := c.Locals("environmentID").(string)
@@ -443,7 +443,7 @@ func Setup(port string) *fiber.App {
 	})
 
 	// Pipeline API Trigger private
-	app.Post("/app/private/api-trigger/:id", auth.ApiAuthMiddle("private"), func(c *fiber.Ctx) error {
+	app.Post("/privateapi/api-trigger/:id", auth.ApiAuthMiddle("private"), func(c *fiber.Ctx) error {
 		c.Accepts("application/json")
 		pipelineID := c.Locals("pipelineID").(string)
 		environmentID := c.Locals("environmentID").(string)
