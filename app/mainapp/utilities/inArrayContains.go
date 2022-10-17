f9337492cec05b16c7e5dbce66c91a117979f7fe
Onat <53895969+onattech@users.noreply.github.com>
2022-10-17 14:49:15 +0300

    Front end websockets paths updated

--- app/mainapp/utilities/inArrayContains.go
+++ app/mainapp/utilities/inArrayContains.go
@@ -7,7 +7,7 @@ Check to see if long string (a) contains any texts contained in the list.
 */
 func InArrayContains(a string, list []string) bool {
 	for _, b := range list {
-		if strings.Contains(a, b) == true {
+		if strings.Contains(b, a) == true {
 			return true
 		}
 	}
