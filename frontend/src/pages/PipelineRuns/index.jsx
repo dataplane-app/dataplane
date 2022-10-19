40785157cf0a4d4467cdc0971f3a4e08c3807dbd
Onat <53895969+onattech@users.noreply.github.com>
2022-10-19 19:30:52 +0300

    Scheduler can now be edit on the pipeline view page

--- frontend/src/pages/PipelineRuns/index.jsx
+++ frontend/src/pages/PipelineRuns/index.jsx
@@ -3,7 +3,7 @@ import { faExpandArrowsAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Box, Drawer, Grid, Typography } from '@mui/material';
 import { useSnackbar } from 'notistack';
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import ReactFlow, { ControlButton, Controls, ReactFlowProvider } from 'react-flow-renderer';
 import { useParams } from 'react-router-dom';
 import CustomLine from '../../components/CustomNodesContent/CustomLine';
@@ -13,7 +13,6 @@ import ViewPageItem from '../../components/MoreInfoContent/ViewPageItem';
 import MoreInfoMenu from '../../components/MoreInfoMenu';
 import { useGlobalPipelineRun } from './GlobalPipelineRunUIState';
 import LogsDrawer from '../../components/DrawerContent/LogsDrawer';
-import TurnOffPipelineDrawer from '../../components/DrawerContent/TurnOffPipelineDrawer';
 import CustomChip from '../../components/CustomChip';
 import { useGetPipeline } from '../../graphql/getPipeline';
 import { Analytics } from './Analytics';
@@ -23,6 +22,7 @@ import { useGlobalRunState } from './GlobalRunState';
 
 import { edgeTypes, nodeTypes } from './NodeTypes';
 import TurnOffPipelineDrawerRunPipeline from '../../components/DrawerContent/TurnOffPipelineDrawerRunPipeline';
+import ScheduleDrawer from '../../components/DrawerContent/SchedulerDrawerRunPage';
 
 const View = () => {
     // Retrieve global environments from drop down - selected environment ID
@@ -129,6 +129,11 @@ const View = () => {
         }
     };
 
+    const onClickElement = useCallback((event, element) => {
+        FlowState.selectedElement.set(element);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
         <Box className="page" height="calc(100vh - 136px)" minHeight="min-content">
             <Box ref={offsetRef}>
@@ -176,6 +181,7 @@ const View = () => {
                                 nodesConnectable={false}
                                 preventScrolling={false}
                                 onLoad={onLoad}
+                                onElementClick={onClickElement}
                                 onConnectStart={onConnectStart}
                                 onConnectEnd={onConnectEnd}
                                 connectionLineComponent={CustomLine}
@@ -220,6 +226,17 @@ const View = () => {
                     environmentID={pipeline?.environmentID}
                     name={pipeline?.name}
                     getPipelines={getPipeline}
+                />
+            </Drawer>
+            <Drawer
+                hideBackdrop
+                sx={{ width: 'calc(100% - 203px)', zIndex: 1099, [`& .MuiDrawer-paper`]: { width: 'calc(100% - 203px)', top: 82 } }}
+                anchor="right"
+                open={FlowState.isOpenSchedulerDrawer.get()}
+                onClose={() => FlowState.isOpenSchedulerDrawer.set(false)}>
+                <ScheduleDrawer
+                    handleClose={() => FlowState.isOpenSchedulerDrawer.set(false)} //
+                    environmentId={Environment.id.get()}
                 />
             </Drawer>
         </Box>
