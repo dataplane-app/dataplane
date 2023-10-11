5f756a5f55a7d5d5272780dfc10db741a778998c
Dataplane Actions <saul-data@users.noreply.github.com>
2023-10-11 20:43:47 +0000

    Automated React build

---
+++ app/mainapp/frontbuild/assets/javascript-1036826a.js
@@ -0,0 +1,6 @@
+import{conf as t,language as e}from"./typescript-e1b6f265.js";import"./index-0693b0a1.js";/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.43.0(94c055bcbdd49f04a0fa15515e848542a79fb948)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/var o=t,r={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{o as conf,r as language};
